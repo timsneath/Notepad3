79b8883d1110965c0fb983604ad19a68750306c0
Tim Sneath <tim@sneath.org>
2014-08-21 12:21:15 -0700

    upload initial project to GitHub

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notepad3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string filename;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //FilenameLabel.DataContext = textEditor.Document;
+        }
+
+        private void CommandNew_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (textEditor.IsModified)
+            {
+                MessageBoxResult mbr = MessageBox.Show("Save changes to \"" + (filename ?? "untitled") + "\"?", "Notepad3", MessageBoxButton.YesNoCancel);
+                if (mbr == MessageBoxResult.Cancel)
+                {
+                    return;
+                }
+
+                if (mbr == MessageBoxResult.Yes)
+                {
+                    CommandSave_Executed(sender, e);
+                }
+                
+                filename = null;
+                textEditor.Clear();
+            }
+        }
+
+        private void CommandOpen_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".txt"; // Default file extension
+            dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension 
+
+            // Process open file dialog box results 
+            if (dlg.ShowDialog() ?? false)
+            {
+                // Open document 
+                filename = dlg.FileName;
+                textEditor.Load(dlg.FileName);
+            }
+        }
+
+        private void CommandSave_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (filename != null)
+            {
+                textEditor.Save(filename);
+            }
+            else
+            {
+                CommandSaveAs_Executed(sender, e);
+            }
+        }
+
+        private void CommandSaveAs_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.DefaultExt = ".txt"; // Default file extension
+            dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension 
+
+            // Process save file dialog box results 
+            if (dlg.ShowDialog() ?? false)
+            {
+                // Save document 
+                filename = dlg.FileName;
+                textEditor.Save(dlg.FileName);
+            }
+        }
+
+        private void CommandExit_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CommandWordWrap_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            textEditor.WordWrap = !textEditor.WordWrap;
+
+        }
+
+        private void CommandShowWhitespace_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            textEditor.Options.ShowSpaces = !textEditor.Options.ShowSpaces;
+            textEditor.Options.ShowTabs = !textEditor.Options.ShowTabs;
+        }
+
+        private void CommandShowLineEndings_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            textEditor.Options.ShowEndOfLine = !textEditor.Options.ShowEndOfLine;
+        }
+
+        private void CommandShowWrapSymbols_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CommandShowLineNumbers_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            textEditor.ShowLineNumbers = !textEditor.ShowLineNumbers;
+        }
+
+        private void CommandAbout_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ NotepadCustomCommands.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Notepad3
+{
+    public class NotepadCustomCommands
+    {
+        static NotepadCustomCommands()
+        {
+            Exit = new RoutedUICommand("E_xit", "Exit", typeof(NotepadCustomCommands),
+                new InputGestureCollection { new KeyGesture(Key.F4, ModifierKeys.Alt, "Alt+F4") });
+
+            WordWrap = new RoutedUICommand("Word W_rap", "WordWrap", typeof(NotepadCustomCommands),
+                new InputGestureCollection { new KeyGesture(Key.W, ModifierKeys.Control, "Ctrl+W") });
+            ShowWhitespace = new RoutedUICommand("Show _Whitespace", "ShowWhitespace", typeof(NotepadCustomCommands),
+                new InputGestureCollection { new KeyGesture(Key.D8, ModifierKeys.Control | ModifierKeys.Shift, "Ctrl+Shift+8") });
+            ShowLineEndings = new RoutedUICommand("Show Line _Endings", "ShowLineEndings", typeof(NotepadCustomCommands),
+                new InputGestureCollection { new KeyGesture(Key.D9, ModifierKeys.Control | ModifierKeys.Shift, "Ctrl+Shift+9") });
+            ShowWrapSymbols = new RoutedUICommand("Show Wrap S_ymbols", "ShowWrapSymbols", typeof(NotepadCustomCommands),
+                new InputGestureCollection { new KeyGesture(Key.D0, ModifierKeys.Control | ModifierKeys.Shift, "Ctrl+Shift+0") });
+            ShowLineNumbers = new RoutedUICommand("Line _Numbers", "ShowLineNumbers", typeof(NotepadCustomCommands),
+                new InputGestureCollection { new KeyGesture(Key.N, ModifierKeys.Control | ModifierKeys.Shift, "Ctrl+Shift+N") });
+
+            About = new RoutedUICommand("_About", "About", typeof(NotepadCustomCommands) );
+        }
+
+        public static RoutedUICommand Exit { get; private set; }
+        public static RoutedUICommand WordWrap { get; private set; }
+        public static RoutedUICommand ShowWhitespace { get; private set; }
+        public static RoutedUICommand ShowLineEndings { get; private set; }
+        public static RoutedUICommand ShowWrapSymbols { get; private set; }
+        public static RoutedUICommand ShowLineNumbers { get; private set; }
+        public static RoutedUICommand About { get; private set; }
+    }
+}
